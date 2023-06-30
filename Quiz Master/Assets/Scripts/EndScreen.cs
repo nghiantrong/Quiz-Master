5958728d8785406b85c1f700a795fc58b5d1c349
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2023-06-30 20:22:30 +0700

    Add empty project

---
+++ Quiz Master/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndScreen : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    ScoreKeeper scoreKeeper;
+
+    
+    void Awake()
+    {
+        scoreKeeper = FindAnyObjectByType<ScoreKeeper>();
+    }
+
+    public void ShowFinalScore()
+    {
+        finalScoreText.text = "Congratulation!\nYou got a score of " 
+                            + scoreKeeper.CalculateScore() + "%";
+    }
+
+
+}
